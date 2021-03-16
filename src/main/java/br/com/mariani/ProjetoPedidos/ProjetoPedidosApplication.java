5fc70eefd62f034cbcd1e19155fedaafe0c84980
Maryucha_Mariani <https://Maryucha_Mariani@bitbucket.org/Maryucha_Mariani/appsensores.git>
2021-03-16 17:19:45 -0300

    subindo o projeto

---
+++ src/main/java/br/com/mariani/ProjetoPedidos/ProjetoPedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.mariani.ProjetoPedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mariani/ProjetoPedidos/ProjetoPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mariani.ProjetoPedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
