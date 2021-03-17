eaffbdc56137ee088810edffcb5eaba6505bdce5
Maryucha_Mariani <https://Maryucha_Mariani@bitbucket.org/Maryucha_Mariani/appsensores.git>
2021-03-17 09:52:19 -0300

    classecategoria

--- src/main/java/br/com/mariani/ProjetoPedidos/resources/CategoriaResource.java
+++ src/main/java/br/com/mariani/ProjetoPedidos/resources/CategoriaResource.java
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.mariani.ProjetoPedidos.domain.Categoria;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String listar() {
+		Categoria categoria1 = new Categoria(1,"informática");
 		return "Rest Está funcionando";
 	}
 
