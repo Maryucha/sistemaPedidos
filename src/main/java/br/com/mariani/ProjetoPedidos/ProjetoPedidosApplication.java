5f67a1c941bb7ebd7d9d1110b696144a44a276cf
Maryucha_Mariani <https://Maryucha_Mariani@bitbucket.org/Maryucha_Mariani/appsensores.git>
2021-03-17 12:03:38 -0300

    operação de instaciação

--- src/main/java/br/com/mariani/ProjetoPedidos/ProjetoPedidosApplication.java
+++ src/main/java/br/com/mariani/ProjetoPedidos/ProjetoPedidosApplication.java
@@ -1,13 +1,32 @@
 package br.com.mariani.ProjetoPedidos;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoPedidosApplication {
+import br.com.mariani.ProjetoPedidos.domain.Categoria;
+import br.com.mariani.ProjetoPedidos.repositories.CategoriaRepositorie;
 
+@SpringBootApplication
+public class ProjetoPedidosApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepositorie repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoPedidosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		repo.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
